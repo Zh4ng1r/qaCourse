6428856adb5faa28b9946ca5cf9f3cff8221fb82
Zhangir <zhangir@MacBook-Pro-Admin.local>
2025-07-30 23:07:59 +0600

    downloaded jest eslint prettier

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,ts}'],
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const transform = {
+  '^.+\\.(ts|tsx)$': 'ts-jest',
+};
+export const moduleFileExtensions = ['ts', 'tsx', 'js', 'json'];
